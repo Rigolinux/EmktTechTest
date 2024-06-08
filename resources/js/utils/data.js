bc6442b1ca797c4dddeb3977e359d818f46d7bc1
rigolinux <rigoberto7567@gmail.com>
2024-06-07 18:09:51 -0600

    add dynamic values

---
+++ resources/js/utils/data.js
@@ -0,0 +1,21 @@
+import img1 from '@/assets/imgs/1.jpg';
+import img2 from '@/assets/imgs/2.jpg';
+import img3 from '@/assets/imgs/3.jpg';
+
+export default [
+  {
+    name: 'Bosque',
+    image: img1,
+    descripcion: 'Descripcion del bosque'
+  },
+  {
+    name: 'Montaña',
+    image: img2,
+    descripcion: 'Descripcion de la montaña'
+  },
+  {
+    name: 'Playa',
+    image: img3,
+    descripcion: 'Descripcion de la playa'
+  }
+];
