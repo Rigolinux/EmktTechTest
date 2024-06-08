d2dfc38246422679853848dc289bd161a1d458d6
rigolinux <rigoberto7567@gmail.com>
2024-06-08 09:23:35 -0600

    translate footer

--- resources/js/Layouts/IndexLayout.vue
+++ resources/js/Layouts/IndexLayout.vue
@@ -29,35 +29,35 @@ import { mdiFacebook,mdiTwitter, mdiInstagram,mdiLinkedin } from '@mdi/js';
     <div class="container p-4">
       <div class="row">
         <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
-          <h5 class="text-uppercase">Contact</h5>
+          <h5 class="text-uppercase">Contacto</h5>
           <ul class="list-unstyled mb-0">
             <li>
-              <a href="#!" class="text-dark">123 Main St, City, Country</a>
+              <a href="#!" class="text-dark">123 SanSalvador, El Salvador</a>
             </li>
             <li>
               <a href="#!" class="text-dark">Email: info@example.com</a>
             </li>
             <li>
-              <a href="#!" class="text-dark">Phone: +1 234 567 890</a>
+              <a href="#!" class="text-dark">Phone: +503 1234 5678</a>
             </li>
           </ul>
         </div>
         <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
-          <h5 class="text-uppercase">Useful Links</h5>
+          <h5 class="text-uppercase">Enlaces de Interes</h5>
           <ul class="list-unstyled">
             <li>
-              <a href="#!" class="text-dark">Privacy Policy</a>
+              <a href="#!" class="text-dark">Politica de Privacidad</a>
             </li>
             <li>
-              <a href="#!" class="text-dark">Terms of Service</a>
+              <a href="#!" class="text-dark">Terminos de el Servicio</a>
             </li>
             <li>
-              <a href="#!" class="text-dark">Support</a>
+              <a href="#!" class="text-dark">Soporte</a>
             </li>
           </ul>
         </div>
         <div class="col-lg-4 col-md-6 mb-4 mb-md-0 ">
-          <h5 class="text-uppercase">Follow Us</h5>
+          <h5 class="text-uppercase">Siguenos</h5>
             <ul class="list-unstyled flex flex-col justify-center">
                 <li class="">
                     <a href="" class="text-dark d-flex align-items-center">
@@ -89,7 +89,7 @@ import { mdiFacebook,mdiTwitter, mdiInstagram,mdiLinkedin } from '@mdi/js';
       </div>
     </div>
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-      © 2024 Company Name
+      © 2024 emkt.company
     </div>
   </footer>
 
